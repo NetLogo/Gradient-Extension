0c3e57a1649f01846e053b3c8f7181e9fa46de2d
TheBizzle <jason.bertsche@gmail.com>
2012-01-27 13:33:14 -0600

    Initial commit

---
+++ src/GradientExtension.java
@@ -0,0 +1,8 @@
+import org.nlogo.api.DefaultClassManager;
+import org.nlogo.api.PrimitiveManager;
+
+public class GradientExtension extends DefaultClassManager {
+    public void load(PrimitiveManager primitiveManager) {
+        primitiveManager.addPrimitive ("scale", new Scale());
+    }
+}
